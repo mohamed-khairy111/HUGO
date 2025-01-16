c9813516b7b7e193c2a2137e04e5cf3b3a530777
mohamed khairy <mohamedkhairy0101@gmail.com>
2025-01-16 20:13:08 +0200

    add the token and tokenizer files

---
+++ engine/parsing/HTML5_Parser/HTMLTokenizer.h
@@ -0,0 +1,101 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class HTMLTokenizer
+{
+
+public:
+    explicit HTMLTokenizer(const string &html) {}
+
+private:
+    string inputBuffer;
+    size_t currentPosition;
+    string currentTokenDate;
+
+    enum class State
+    {
+        Data,
+        RCDATA,
+        RAWTEXT,
+        ScriptData,
+        PlainText,
+        TagOpen,
+        EndTagOpen,
+        TagName,
+        RCDATALessThanSign,
+        RCDATAEndTagOpen,
+        RCDATAEndTagName,
+        RAWTEXTLessThanSign,
+        RAWTEXTEndTagOpen,
+        RAWTEXTEndTagName,
+        ScriptDataLessThanSign,
+        ScriptDataEndTagOpen,
+        ScriptDataEndTagName,
+        ScriptDataEscapeStart,
+        ScriptDataEscapeStartDash,
+        ScriptDataEscaped,
+        ScriptDataEscapedDash,
+        ScriptDataEscapedDashDash,
+        ScriptDataEscapedLessThanSign,
+        ScriptDataEscapedEndTagOpen,
+        ScriptDataEscapedEndTagName,
+        ScriptDataDoubleEscapeStart,
+        ScriptDataDoubleEscaped,
+        ScriptDataDoubleEscapedDash,
+        ScriptDataDoubleEscapedDashDash,
+        ScriptDataDoubleEscapedLessThanSign,
+        ScriptDataDoubleEscapeEnd,
+        BeforeAttributeName,
+        AttributeName,
+        AfterAttributeName,
+        BeforeAttributeValue,
+        AttributeValueDoubleQuoted,
+        AttributeValueSingleQuoted,
+        AttributeValueUnquoted,
+        AfterAttributeValueQuoted,
+        SelfClosingStartTag,
+        BogusComment,
+        MarkupDeclarationOpen,
+        CommentStart,
+        CommentStartDash,
+        Comment,
+        CommentLessThanSign,
+        CommentLessThanSignBang,
+        CommentLessThanSignBangDash,
+        CommentLessThanSignBangDashDash,
+        CommentEndDash,
+        CommentEnd,
+        CommentEndBang,
+        DOCTYPE,
+        BeforeDOCTYPEName,
+        DOCTYPEName,
+        AfterDOCTYPEName,
+        AfterDOCTYPEPublicKeyword,
+        BeforeDOCTYPEPublicIdentifier,
+        DOCTYPEPublicIdentifierDoubleQuoted,
+        DOCTYPEPublicIdentifierSingleQuoted,
+        AfterDOCTYPEPublicIdentifier,
+        BetweenDOCTYPEPublicAndSystemIdentifiers,
+        AfterDOCTYPESystemKeyword,
+        BeforeDOCTYPESystemIdentifier,
+        DOCTYPESystemIdentifierDoubleQuoted,
+        DOCTYPESystemIdentifierSingleQuoted,
+        AfterDOCTYPESystemIdentifier,
+        BogusDOCTYPE,
+        CDATASection,
+        CDATASectionBracket,
+        CDATASectionEnd,
+        CharacterReference,
+        NamedCharacterReference,
+        AmbiguousAmpersand,
+        NumericCharacterReference,
+        HexadecimalCharacterReferenceStart,
+        DecimalCharacterReferenceStart,
+        HexadecimalCharacterReference,
+        DecimalCharacterReference,
+        NumericCharacterReferenceEnd
+    };
+
+    State state;
+};
