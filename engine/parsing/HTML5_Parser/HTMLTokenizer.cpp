e8143a5f24e6c7237afd0398afa9b795036367f2
mohamed-khairy111 <mohamedkhairy0101@gmail.com>
2025-02-01 20:55:27 +0200

    working on the tokenizer

--- engine/parsing/HTML5_Parser/HTMLTokenizer.cpp
+++ engine/parsing/HTML5_Parser/HTMLTokenizer.cpp
@@ -8,49 +8,50 @@ HTMLTokenizer::HTMLTokenizer(const string &html)
       state(State::Data),
       return_state(State::Data) {}
 
-void HTMLTokenizer::tokenize()
-{
+void HTMLTokenizer::tokenize() {
     optional<char> currentChar = next_character();
 
-    switch (state)
-    {
+    switch (state) {
     case State::Data:
-        if (currentChar.has_value() && currentChar.value() == '&')
-        {
+        if (currentChar.has_value() && currentChar.value() == '&') {
             return_state = State::Data;
             switchState(State::CharacterReference);
         }
 
-        if (currentChar.has_value() && currentChar.value() == '>')
-        {
+        if (currentChar.has_value() && currentChar.value() == '>') {
             switchState(State::TagOpen);
         }
         break;
 
     case State::TagOpen:
-        if (currentChar.has_value() && currentChar.value() == '!')
-        {
+        if (currentChar.has_value() && currentChar.value() == '!') {
             switchState(State::MarkupDeclarationOpen);
         }
         break;
 
     case State::MarkupDeclarationOpen:
-        if (next_few_character_are("DOCTYPE"))
-        {
+        if (next_few_character_are("DOCTYPE")) {
             consume("DOCTYPE");
             switchState(State::DOCTYPE);
         }
         break;
 
     case State::DOCTYPE:
-        if (currentChar.has_value() && (currentChar.value() == '\t' || currentChar.value() == '\a' || currentChar.value() == '\f' || currentChar.value() == ' '))
-        {
+        if (currentChar.has_value() &&
+            (currentChar.value() == '\t' || currentChar.value() == '\a' ||
+             currentChar.value() == '\f' || currentChar.value() == ' ')) {
             switchState(State::BeforeDOCTYPEName);
         }
 
         break;
 
     case State::BeforeDOCTYPEName:
+        if (currentChar.has_value() &&
+            (currentChar.value() == '\t' || currentChar.value() == '\a' ||
+             currentChar.value() == '\f' || currentChar.value() == ' ')) {
+            switchState(State::BeforeDOCTYPEName);
+        } else {
+        }
 
         break;
 
@@ -59,40 +60,30 @@ void HTMLTokenizer::tokenize()
     }
 }
 
-char HTMLTokenizer::next_character()
-{
-    if (currentPosition >= inputBuffer.length())
-    {
+char HTMLTokenizer::next_character() {
+    if (currentPosition >= inputBuffer.length()) {
         return 0;
     }
 
     return inputBuffer[currentPosition++];
 }
-char HTMLTokenizer::peek_character(int offset) const
-{
-    if (currentPosition + offset >= inputBuffer.length())
-    {
+char HTMLTokenizer::peek_character(int offset) const {
+    if (currentPosition + offset >= inputBuffer.length()) {
         return 0;
     }
 
     return inputBuffer[currentPosition + offset];
 }
-void HTMLTokenizer::consume(const string s)
-{
-    currentPosition += s.length();
-}
+void HTMLTokenizer::consume(const string s) { currentPosition += s.length(); }
 
-void HTMLTokenizer::switchState(State newState)
-{
+void HTMLTokenizer::switchState(State newState) {
     state = newState;
 
     optional<char> currentChar = next_character();
 }
 
-bool HTMLTokenizer::next_few_character_are(const string s) const
-{
-    for (int i = 0; i < s.length(); i++)
-    {
+bool HTMLTokenizer::next_few_character_are(const string s) const {
+    for (int i = 0; i < s.length(); i++) {
         optional<char> character = peek_character(i);
         if (!character.has_value())
             return false;
